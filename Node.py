8513f391c1ee99a0a337e0ab36a124f0c8158fac
Julie Meglio <118858432+spritzly@users.noreply.github.com>
2023-09-04 20:39:16 -0400

    Added Node.py
    Added Node.py, the reference file imported into all other files in this repository

---
+++ Node.py
@@ -0,0 +1,27 @@
+'''
+This is the basis for other files used in data structure/OOP practice files,
+sorta like a boilerplate so I don't have to write this shit over and over again.
+If neeeded, I will import the Node class from this file onto the file I will be
+currently working on :)
+'''
+
+class Node:
+  def __init__(self, value, next_node=None, prev_node=None):
+    self.value = value
+    self.next_node = next_node
+    self.prev_node = prev_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+
+  def set_prev_node(self, prev_node):
+    self.prev_node = prev_node
+    
+  def get_prev_node(self):
+    return self.prev_node
+  
+  def get_value(self):
+    return self.value
